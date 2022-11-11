8e1a838f02e86368bd68a77cf568850940a832e0
Abhishek0090 <palabhishek411@gmail.com>
2022-11-11 11:58:42 +0530

    added backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import http from '../http'
+import axios from "axios";
 
 const Home = () => {
-  const [user, setuser] = useState(0)
-  return (
+  const [user, setUser] = useState('')
+  const baseURL = "http://localhost:8000/api";
+  
+  const fetchAllUser = ()=>{
+    
+    axios.get(baseURL).then((response) => {
+      setUser(response.data);
+    });
+  }
+
+    useEffect(() => {
+      fetchAllUser();
+    }, [])
+     
+    return (
     <div>
-      home
+      <h2>User listing..</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Sno</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        {user.map((user,index)=>{
+ 
+          <tr key={user.id}>
+            <td>{index}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>edit</td>
+             
+          </tr>
+        })}
+        </tbody>
+      </table>
     </div>
   )
 }
