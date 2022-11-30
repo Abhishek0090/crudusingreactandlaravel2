398cf5169cf96ac23ba913fa0ff3c3908af32d8e
Abhishek0090 <palabhishek411@gmail.com>
2022-11-30 14:31:17 +0530

    changes alot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,22 +15,22 @@ import Edit from './pages/Edit';
 function App() {
   return (
     <>
-     <nav class="navbar navbar-expand-lg  bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Crud App</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+     <nav className="navbar navbar-expand-lg  bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" to="/">Crud App</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/create">Create</Link>
+            <li className="nav-item">
+              <Link className="nav-link" to="/create">Create</Link>
             </li> 
-            <li class="nav-item">
-              <Link class="nav-link" to="/edit">Edit</Link>
+            <li className="nav-item">
+              <Link className="nav-link" to="/edit">Edit</Link>
             </li> 
         </ul> 
       </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,44 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import http from '../http'
-import axios from "axios";
+ 
 
 const Home = () => {
-  const [user, setUser] = useState('')
-  const baseURL = "http://localhost:8000/api";
+  const [user, setUsers] = useState('')
+  // const baseURL = "http://localhost:8000/api";
   
-  const fetchAllUser = ()=>{
-    
-    axios.get(baseURL).then((response) => {
-      setUser(response.data);
-    });
-  }
 
-    useEffect(() => {
-      fetchAllUser();
-    }, [])
-     
+  useEffect(()=>{
+    fetchAllUsers();
+},[]);
+
+const fetchAllUsers = () => {
+    http.get('/users').then(res=>{
+        setUsers(res.data);
+    })
+}
+
+    
     return (
     <div>
       <h2>User listing..</h2>
-      <table>
+      <table className='table'>
         <thead>
           <tr>
-            <th>Sno</th>
+            <th>No</th>
             <th>Name</th>
             <th>Email</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
-        {user.map((user,index)=>{
- 
+        {Array.from(user).map((user,index)=>{
+          return( 
           <tr key={user.id}>
             <td>{index}</td>
             <td>{user.name}</td>
             <td>{user.email}</td>
-            <td>edit</td>
-             
+            <td>edit</td> 
           </tr>
+        )
         })}
         </tbody>
       </table>
